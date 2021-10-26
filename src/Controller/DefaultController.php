dec746ace7e840ce73b74fa76be1718428944d11
alexandreleao <erdleosalgueiro@gmail.com>
2021-10-26 16:06:26 -0300

    Implementacao de controller e templates

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,12 +5,11 @@
    use Symfony\Component\HttpFoundation\Response;
    use Symfony\Component\HttpFoundation\Request;
    use Symfony\Component\Routing\Annotation\Route;
-
+  
    class DefaultController
    {
-      /**
-       * @Route("/")
-       */
+
+      
        public function index(Request $request) : Response
        {
          $resp = new Response();
---
+++ src/Controller/UsuarioController.php
@@ -0,0 +1,33 @@
+<?php
+ 
+ namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+ use Symfony\Component\Routing\Annotation\Route;
+
+ /**
+  *  @Route("/", name="web_usuario_")
+  */
+
+  class UsuarioController extends AbstractController
+ {
+    /**
+     * @Route("/", methods={"GET"}, name="index")
+     */
+
+    public function index(): Response 
+    {
+        return $this->render("usuario/form.html.twig");
+
+    }
+
+   /**
+    * @Route("/salvar", methods={"POST"}, name="salvar")
+    */
+
+    Public function salvar(): Response
+    {
+        return new Response("Implementar gravação ao banco de dados");
+    }
+}
